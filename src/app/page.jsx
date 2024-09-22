f1a83ae62794fa144cd6460ab67b539f21a0ef65
Alejandra Garza <99775794+aleegaarza@users.noreply.github.com>
2024-09-22 09:56:28 -0600

    added firebase package

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>User management panel</div>
+  )
+}
+
+export default HomePage
---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+  // Signed up
+  const user = userCredential.user;
+  // ...
+});
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
