aa045440336f0687699cb73d9491cbe90567c99b
Alejandra Garza <99775794+aleegaarza@users.noreply.github.com>
2024-10-23 15:50:56 -0600

    added observer
    added an authentication state observer to show user data when logged in

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,23 @@
 "use client";
-import { firestore } from "@/firebase/firebase";
+import { auth, firestore } from "@/firebase/firebase";
 import React, { useEffect, useState } from "react";
 import { Table, Button, Form } from "react-bootstrap";
 import app from "../firebase/firebase";
 import LoginPage from "./auth/login/page";
+import ProfilePage from "./profile/page";
 
 function HomePage() {
+  const [user, setUser] = useState()
+  useEffect(() =>{
+    auth.onAuthStateChanged(user=>{
+      setUser(user)
+    })
+  })
   return (
     <div>
       <h2>Welcome</h2>
-      <LoginPage/>
+      {user ? <ProfilePage/> : <LoginPage/>}
+      
     </div>
   );
 }
